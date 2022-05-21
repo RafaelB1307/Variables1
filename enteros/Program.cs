a06ca62ea414b8271dff7700e2d7fccc330d9095
RafaelB1307 <Rafael.bastides@uabc.edu.mx>
2022-05-21 00:32:45 -0700

    Agregar archivos de proyecto.

---
+++ enteros/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enteros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 54;
+            int num3 = 20;
+            int total = num1 + num2 + num3;
+            Console.WriteLine("el valor de suma es:" + total);
+            Console.WriteLine("presiona cualquier tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
